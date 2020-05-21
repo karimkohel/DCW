176f3e25bed03dd20b09480565d3c8d7183d827f
karim kohel <kareemkohel@gmail.com>
2020-05-21 22:14:50 +0200

    Started the starter C file and Wrote a bio for clearing anything that wasn't clear enough, (tho im sure everything was)

---
+++ DCW.c
@@ -0,0 +1,17 @@
+/*
+Date and README for people who can't use github :
+
+	 				May 21, 2020
+
+The Delightful Compression Wizard is a command line app that takes in an input text file
+(has to be named input.txt) and compress it losslessly using Huffman code.
+The code will be WET, it is a requirement that we only use C/C++ files and no header file can comply with that.
+I will try to maintain basic DRY principle around the file and perhaps organize it as much as humanly possible.
+Github link 
+(Where my commits live in chronological order with every lil bit of change in the code to make sure i didn't plagiarize)
+: 										github.com/karimkohel/DCW
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "DCW_.h"
