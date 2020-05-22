902f1dc39988de450358904a6660c3506c8f2b40
karim kohel <kareemkohel@gmail.com>
2020-05-22 20:04:28 +0200

    getting the input file and trying to count the characters in it

--- general.c
+++ general.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
+// Read input FILE and load it to memory
 FILE *get_input(const char *file_name){
-	
+
+	FILE *file = fopen(file_name, "r");
+	int total_char = 0;
+	char c;
+
+	while((c=fgetc(file)) != EOF){
+		//how do i count tho...
+	}
+
 }
