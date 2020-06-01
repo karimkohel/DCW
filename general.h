6c676dd21aeca5655a83d329dc524e4d5c0691bf
karim kohel <kareemkohel@gmail.com>
2020-06-01 15:39:07 +0200

    was working on the encodeing of characters step when i realised i need a hash table

--- general.h
+++ general.h
@@ -24,8 +24,6 @@ void load_file_in(Q_t *q, const char *file_name, int *count){
 	fclose(file);
 }
 
-
-
 // //plagiarized function do not use
 // #include <errno.h>
 // #include <string.h>
@@ -39,5 +37,32 @@ void load_file_in(Q_t *q, const char *file_name, int *count){
 //     }
 //     return sb.st_size;
 // }
+
+
+void encode(node_t *tree, node_t **head, node_t **tail, char code[]){
+
+	if(!tree)
+		return; // exit case for the nodes after the leafnodes
+
+	if(leaf_node(tree)){
+		insert_first(head, tail, )
+	}
+}
+
+//function that is responsible for getting all huffman codes from a huffman tree
+// this function will be documented as it won't be self explanatory enough (because it's probably ugly)
+// sorry for the spaghetti code
+bool get_codes(node_t **head, node_t *tree){
+
+	if((*head) != NULL)
+		return false; // we need an empty list to start
+
+	node_t *tmp_head = NULL;
+	node_t *tmp_tail = NULL;
+
+	encode(tree, (&tmp), (&tmp_tail), str);
+
+}
+
 
 #endif
--- tree.h
+++ tree.h
@@ -1,96 +1,13 @@
 #ifndef TREE
 #define TREE
 
-node_t *insert(node_t *root, char data){
-
-	if(root == NULL){
-		root = create_node(data);
-		return root;
-	}
-
-	if(data > (root->data))
-		root->right = insert(root->right, data);
-	else if(data < (root->data))
-		root->left = insert(root->left, data);
-
-	return root;
-}
-
-node_t *find(node_t *root, char data){
-
-	if(root == NULL)
-		return NULL;
-
-	if(data > (root->data))
-		return find(root->right, data);
-	else if(data < (root->data))
-		return find(root->left, data);
-	else
-		return root;
-}
-
-node_t *find_max(node_t *root){
-
-	if(root == NULL)
-		return NULL;
-
-	if(root->right)
-		return find_max(root->right);
-	else
-		return root;
-}
-
-node_t *find_min(node_t *root){
-	if(root == NULL)
-		return NULL;
-
-	if(root->left)
-		return find_max(root->left);
-	else
-		return root;
-}
+// void delete_node()//pass
 
 bool leef_node(node_t *node){
 	if((node->right) == NULL && (node->left) == NULL)
 		return true;
 	else
 		return false;
-}
-
-node_t *delete_node(node_t *node, char data){
-
-	if(node == NULL)
-		return NULL;
-
-	if(data > (node->data))
-		node->right = delete_node(node->right, data);
-	else if(data < (node->data))
-		node->left = delete_node(node->left, data);
-	else{
-
-		if(leef_node(node)){
-			free(node);
-			node = NULL;
-			return node;
-		}
-		else if((node->left) == NULL){
-			node_t *tmp = node;
-			node = node->right;
-			free(tmp);
-		}
-		else if((node->right) == NULL){
-			node_t *tmp = node;
-			node = node->left;
-			free(tmp);
-		}
-		else{
-			node_t *tmp = find_min(node->right);//or find_max in left Sub-tree
-			node->data = tmp->data;
-			node->right = delete_node(node->right, tmp->data);//to remove duplicate
-		}
-	}
-
-	return node;
 }
 
 node_t *join_nodes(node_t *node1, node_t *node2){
@@ -103,14 +20,26 @@ node_t *join_nodes(node_t *node1, node_t *node2){
 	new_node->right = node2;
 	new_node->freq = (node1->freq) + (node2->freq);
 
-	printf("\nfirst-%d, second-%d total-%d\n", (node1->freq), (node2->freq), new_node->freq);
-
 	return new_node;
 }
 
-// void get_codes_from_tree(node_t *root){
+// // void get_codes_from_tree(node_t *root){
 	
+// // }
+
+// char *encode_tree(node_t *node, char code[]){
+
+// 	if(leef_node(node)){
+// 		delete_node(node);
+// 		return code;
+// 	}
+
+// 	char *tmp = code;
+// 	encode_tree(node->left, strcat(code, "0"));
+// 	encode_tree(node->right, strcat(tmp, "1"));
 // }
+
+
 
 void printInorder(node_t *root){
 
